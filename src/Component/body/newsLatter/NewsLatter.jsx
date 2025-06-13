17b6937d40c4953ce44ec0f88c99be6b311c2ba3
Minhajh2o <h2o.minhaj@gmail.com>
2025-06-13 13:01:46 +0600

    basic components and project structer created.

---
+++ src/Component/body/newsLatter/NewsLatter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsLatter = () => {
+    return (
+        <div>
+            <h1>BPL News Letter</h1>
+        </div>
+    );
+};
+
+export default NewsLatter;
---
+++ src/Component/body/selectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedPlayers = () => {
+    return (
+        <div>
+            <h1>BPL Selected Players</h1>
+        </div>
+    );
+};
+
+export default SelectedPlayers;
