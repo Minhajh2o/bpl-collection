ce1989a8ade7d716e9853aeb4b08e4c993f544b2
Minhajh2o <h2o.minhaj@gmail.com>
2025-06-22 18:40:40 +0600

    navbar, banner and player section made responsive for small device

--- src/Component/body/players/Players.jsx
+++ src/Component/body/players/Players.jsx
@@ -18,8 +18,8 @@ const Players = () => {
   return (
     <div className="mb-24">
       <div className="flex flex-col md:flex-row justify-between items-center gap-4 mb-8">
-        <h3 className="hidden text-[28px] font-semibold">Available Players</h3>
-        <h3 className="text-xl md:text-[28px] font-semibold">Selected Players ({countSelected}/6)</h3>
+        <h3 className="text-xl text-[28px] font-semibold">Available Players</h3>
+        <h3 className="hidden text-xl md:text-[28px] font-semibold">Selected Players ({countSelected}/6)</h3>
         {/* Toggle buttons */}
         <div className="flex items-center justify-around w-full md:w-auto border border-gray-300 rounded-xl">
           <button className="cursor-pointer py-4 px-8 rounded-l-xl">
