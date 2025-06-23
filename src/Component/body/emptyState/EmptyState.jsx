87691292e3de85fadc2bc92bc0e7573e3d9ae14a
Minhajh2o <h2o.minhaj@gmail.com>
2025-06-23 23:48:26 +0600

    empty alert icon size increased

--- src/Component/body/emptyState/EmptyState.jsx
+++ src/Component/body/emptyState/EmptyState.jsx
@@ -5,7 +5,7 @@ const EmptyState = ({ toggleActiveState }) => {
         <div>
         <div className="flex flex-col items-center justify-center text-center mt-12 md:mt-16">
           {/* Alert Icon */}
-          <figure className="h-64 w-64 mx-auto mb-6">
+          <figure className="h-72 w-72 mx-auto mb-6">
             <img src={alertIcon} alt="" />
           </figure>
           <h3 className="text-3xl text-gray-600 font-semibold">
