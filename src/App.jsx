80120eac0c832ebb91615b767b3685e7e61726c4
Minhajh2o <h2o.minhaj@gmail.com>
2025-06-23 16:40:55 +0600

    functions optimized

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import NewsLatter from "./Component/body/newsLatter/NewsLatter";
 import Players from "./Component/body/players/Players";
-import Footer from "./Component/footer/footer";
+import Footer from "./Component/footer/Footer";
 import Banner from "./Component/header/banner/Banner";
 import Navbar from "./Component/header/navbar/Navbar";
 import SelectedPlayers from "./Component/body/selectedPlayers/SelectedPlayers";
@@ -26,18 +26,12 @@ function App() {
 
   // Add 1 million coins
   const addCoin = () => {
-    const newCoin = coin + 1_000_000;
-    setCoin(newCoin);
+    setCoin((coin) => coin + 1_000_000);
   };
 
   // Subtract coins based on player price
   const subtractCoin = (price) => {
-    const newCoin = coin - price;
-    if (newCoin >= 0) {
-      setCoin(newCoin);
-    } else {
-      alert("You don't have enough coins to subtract.");
-    }
+    setCoin((coin) => (coin - price >= 0 ? coin - price : coin));
   };
 
   // Add selected player to the list
@@ -47,20 +41,24 @@ function App() {
     if (alreadySelected) {
       return;
     }
-    if (selectedPlayers.length < 6) {
-      setSelectedPlayers([...selectedPlayers, player]);
-      subtractCoin(player.price);
-    } else {
+    if (selectedPlayers.length >= 6) {
       alert("You can only select up to 6 players.");
+      return;
     }
+    if (coin < player.price) {
+      alert("You don't have enough coins to buy this player.");
+      return;
+    }
+    setSelectedPlayers((selectedPlayers) => [...selectedPlayers, player]);
+    subtractCoin(player.price);
   };
 
   // app render
   return (
     <>
-        <div className="sticky top-0 z-50 bg-white">
-          <Navbar coin={coin} />
-        </div>
+      <div className="sticky top-0 z-50 bg-white">
+        <Navbar coin={coin} />
+      </div>
       <div className="container max-w-7xl mx-auto">
         <div className="px-5">
           <Banner addCoin={addCoin} />
